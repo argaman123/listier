531cde415232483667db12397b077653c6f1b082
argam <argaman.gez@gmail.com>
2025-01-03 12:28:51 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { ButtonDemoComponent } from './button-demo/button-demo.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      },
+      ripple: true
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/button-demo/button-demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button-demo',
+  templateUrl: './button-demo.component.html',
+  styleUrl: './button-demo.component.scss',
+  standalone: false
+})
+export class ButtonDemoComponent {}
